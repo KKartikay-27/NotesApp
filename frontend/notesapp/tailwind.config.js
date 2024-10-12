aeed9af63dc8dc418f7ec2d8a35184b8539e432c
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-10-13 01:52:19 +0530

    initial commit/ added ui of login

---
+++ frontend/notesapp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#2B85FF",
+        secondary : "#EF863E"
+      }
+    },
+  },
+  plugins: [],
+}
+
