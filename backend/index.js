c99f1a716b7080d3773ea8b20ceeebaf6c10ec75
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-10-14 23:45:11 +0530

    added login functionality

--- backend/index.js
+++ backend/index.js
@@ -44,12 +44,12 @@ app.post('/create-account', async (req, res) => {
         return res.status(400).json({ error: true, message: "Password is required" });
     }
 
-    const isUser = await User.findOne({email : email});
+    const isUser = await User.findOne({ email: email });
 
-    if(isUser){
+    if (isUser) {
         return res.json({
-            error:true,
-            message : "User already exists"
+            error: true,
+            message: "User already exists"
         });
     }
 
@@ -73,6 +73,44 @@ app.post('/create-account', async (req, res) => {
         message: "User created successfully",
     });
 
+});
+
+//Login
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Password is required" });
+    }
+
+    const userInfo = await User.findOne({ email: email });
+
+    if (!userInfo) {
+        return res.status(400).json({ error: true, message: "User not found" });
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo };
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m",
+        });
+
+        return res.json({
+            error: false,
+            message: "Login successful",
+            email,
+            accessToken,
+        });
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: "Invalid credentials"
+        });
+    }
 });
 
 app.listen(8000);
