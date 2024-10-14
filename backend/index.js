931783af5461b87409a761fbf774478a7e5f7948
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-10-14 23:35:27 +0530

    added create account functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require('mongoose');
+
+mongoose.connect(config.connectionString);
+
+const User = require('./models/user.model')
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+
+const jwt = require('jsonwebtoken');
+const { authenticateToken } = require('./utilities');
 
 app.use(express.json());
 
@@ -10,8 +23,56 @@ app.use(
     })
 );
 
-app.get('/', (req,res) =>{
-    res.json({data : "Hello" });
+app.get('/', (req, res) => {
+    res.json({ data: "Hello" });
+});
+
+//Create a new user
+app.post('/create-account', async (req, res) => {
+
+    const { fullname, email, password } = req.body;
+
+    if (!fullname) {
+        return res.status(400).json({ error: true, message: "Fullname is required" });
+    }
+
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Password is required" });
+    }
+
+    const isUser = await User.findOne({email : email});
+
+    if(isUser){
+        return res.json({
+            error:true,
+            message : "User already exists"
+        });
+    }
+
+
+    const user = new User({
+        fullname,
+        email,
+        password,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m",
+    })
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "User created successfully",
+    });
+
 });
 
 app.listen(8000);
